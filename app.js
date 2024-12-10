3ade58bf47d901e1c12f00a4d6ef44f87b5658ed
Vagramos <vag.pare@gmail.com>
2024-12-10 20:45:25 -0300

    Novas funções

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -12,6 +14,10 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
-function verificarChute(params) {
-    console.log('Clicou!');
+function verificarChute() {
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10) + 1;
 }
